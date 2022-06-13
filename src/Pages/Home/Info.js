10ab9f1dbafa00ff38040649c54fb8d64f25422d
ctasbihas <tasbihhmd@gmail.com>
2022-06-13 22:43:58 +0600

    adding services in home page

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' description="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock} />
             <InfoCard bgClass='bg-accent' description="Brooklyn, NY 10036, United States" cardTitle="Visit out location" img={marker} />
             <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' description="+000 123 456789" cardTitle="Contact us now" img={phone} />
---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Service from './Service';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import DentalCare from './DentalCare';
+
+const Services = () => {
+
+    const services = [
+        {
+            _id: 1,
+            img: fluoride,
+            name: 'Fluoride Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+        {
+            _id: 2,
+            img: cavity,
+            name: 'Cavity Filling',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        },
+        {
+            _id: 3,
+            img: whitening,
+            name: 'Teeth Whitening',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the'
+        }
+    ];
+
+    return (
+        <div className='mt-32 mb-10'>
+            <div className='text-center uppercase'>
+                <h2 className='text-secondary text-xl font-bold'>Our Services</h2>
+                <h1 className='text-4xl'>Services we provide</h1>
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    />)
+                }
+            </div>
+            <div className='mt-24'>
+                <DentalCare/>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
