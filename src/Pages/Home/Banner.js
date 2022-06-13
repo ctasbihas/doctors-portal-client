ed7d23d71b28ea70b257b72e1bdc93d87d1f29f5
ctasbihas <tasbihhmd@gmail.com>
2022-06-13 16:19:52 +0600

    adding information card in home page

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,11 +5,11 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen px-12">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={chair} className="rounded-lg shadow-2xl w-[600px]" />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-                    <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
-                    <button className="btn btn-primary text-white bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
                 </div>
             </div>
         </div>
