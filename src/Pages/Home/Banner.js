5da38568a54f57a7a830750e362210800427a965
ctasbihas <tasbihhmd@gmail.com>
2022-06-13 15:30:03 +0600

    adding banner in home page

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen px-12">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-primary text-white bg-gradient-to-r from-secondary to-primary">Get Started</button>
                 </div>
             </div>
         </div>
