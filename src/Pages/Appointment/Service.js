2ddf23e9b81ba9c65681835068672f0fd3cb755a
ctasbihas <tasbihhmd@gmail.com>
2022-06-22 05:00:04 +0600

    h

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,21 +5,21 @@ const Service = ({ service, setTreatment }) => {
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body text-center">
-                    <h2 className="text-xl font-bold text-secondary">{name}</h2>
-                    <p className='text-sm'>
-                        {
-                            slots.length
-                                ? <span>{slots[0]}</span>
-                                : <span>No slots available today</span>
-                        }
-                    </p>
-                    <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <h2 className="text-xl font-bold text-secondary">{name}</h2>
+                <p className='text-sm'>
+                    {
+                        slots.length
+                            ? <span>{slots[0]}</span>
+                            : <span>No slots available today</span>
+                    }
+                </p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
                         for="booking-modal"
-                        class="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Book appointment</label>
+                        className="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Book appointment</label>
                 </div>
             </div>
         </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,83 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    let errorMessage;
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+    if (user) {
+        console.log(user.user);
+    }
+    if (error) {
+        errorMessage = <div className="alert alert-error shadow-lg"><div><svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span className='text-sm'>{error.message}</span></div></div>
+    }
+
     return (
-        <div className=''>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+        <div className='flex  justify-center items-center'>
+            <div className="hero  w-1/2">
+                <div className="hero-content flex-col w-full">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <h2 className="text-3xl font-bold text-center">Login</h2>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Email address"
+                                        className="input input-bordered"
+                                        {...register("email", {
+                                            pattern: {
+                                                value: /[A-Za-z]{3}/,
+                                                message: 'error message'
+                                            }
+                                        })}
+                                    />
+                                    <label class="label">
+                                        <span class="label-text-alt ">{errors.email?.type === 'required' && "Email is required"}</span>
+                                    </label>
+                                </div>
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input
+                                        type="password"
+                                        placeholder="Password"
+                                        className="input input-bordered"
+                                        {...register("password", { required: true })}
+                                    />
+                                    <label class="label">
+                                        <span class="label-text-alt ">{errors.password && "Password is required"}</span>
+                                    </label>
+                                    <label className="label my-2">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+
+                                <div className="form-control">
+                                    <button type='submit' className="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                            <label className="label">
+                                New to Doctors Portal? <a href="#" className="text-secondary">Create new account</a>
+                            </label>
+                            <div className="divider">OR</div>
+                            {errorMessage}
+                            <button
+                                onClick={() => signInWithGoogle()}
+                                className={loading ? "btn btn-outline loading" : "btn btn-outline"}>
+                                CONTINUE WITH GOOGLE
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
